d5fc8d27c8bbc47fbc68d165ad114d8461343b4c
ehu009 <jenssen.eirik@gmail.com>
2020-04-07 19:13:21 +0200

    class interfaces for cards and decks

--- cards.py
+++ cards.py
@@ -1,6 +1,28 @@
 
-class Deck(object):
-  pass
+  
   
 class Card(object):
-  pass
+    
+  def __init__(self, value):
+    pass
+    
+  def __repr__(self):
+    pass
+    
+  def __int__(self):
+    pass
+    
+
+class Deck(object):
+  
+  def __init__(self, deck_path=None):
+    pass
+  
+  def __len__(self):
+    pass
+    
+  def pick(self, n=1):
+    pass
+  
+  def export(self, save_path):
+    pass
