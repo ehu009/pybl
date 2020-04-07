cd4e17309c13cc1fc4f5f03c497b83672dccf2f4
ehu009 <jenssen.eirik@gmail.com>
2020-04-07 22:35:42 +0200

    class interface for participants

---
+++ roles.py
@@ -0,0 +1,20 @@
+
+class Participant(object):
+  def __init__(self):
+    pass
+  
+  def __repr__(self):
+    pass
+  
+  def __int__(self):
+    pass
+  
+  def turn(self, deck):
+    pass
+
+
+class Dealer(Participant):
+  pass
+
+class Player(Participant):
+  pass
