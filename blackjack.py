c3639f7e5430c5476cce94d782ccbe3d6955612c
ehu009 <jenssen.eirik@gmail.com>
2020-04-08 18:29:21 +0200

    empty class interface for a Game

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,27 @@
 
+from helpers import *
+from cards import *
+from roles import *
+
+
+class EmptyDeckError(Exception):
+  pass
+
+
+class Game(object):
+  def __init__(self, player_list, deck_path=None):
+    pass
+
+  def participants():
+    pass
+  
+  def conclude():
+    pass
+  
+  def end():
+    pass
+
+
 def new_game(player_names, deck_path):
   pass
   
