4e21be35365223290843b9ad93a028fd3ba29875
ehu009 <jenssen.eirik@gmail.com>
2020-04-08 03:06:44 +0200

    implements game participants

--- roles.py
+++ roles.py
@@ -1,16 +1,30 @@
 
 class Participant(object):
-  def __init__(self):
-    pass
+  def __init__(self, name):
+    self.name = name
+    self.playing = True
+    self.cards = []
+    self.score = 0
+  
+  def __bool__(self):
+    return self.playing
   
   def __repr__(self):
-    pass
+    return self.name + ": " + ", ".join(list(map(lambda x: repr(x), self.cards)))
   
   def __int__(self):
-    pass
+    return self.score
   
-  def turn(self, deck):
-    pass
+  def __len__(self):
+    return len(self.cards)
+  
+  def take(self, card):
+    self.cards.append(card)
+    self.score += int(card)
+    if self.score > 21:
+      self.playing = False
+  
+
 
 
 class Dealer(Participant):
